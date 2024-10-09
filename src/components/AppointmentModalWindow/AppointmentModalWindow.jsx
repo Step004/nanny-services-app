dc94a3b081965bfdbd949a9e3f2060361dcd497f
Step004 <ctepan.filep9@gmail.com>
2024-10-09 21:51:09 +0300

    finish with form

--- src/components/AppointmentModalWindow/AppointmentModalWindow.jsx
+++ src/components/AppointmentModalWindow/AppointmentModalWindow.jsx
@@ -4,15 +4,26 @@ import * as Yup from "yup";
 import { IoClose } from "react-icons/io5";
 import { FaRegClock } from "react-icons/fa";
 import clsx from "clsx";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function AppointmentModalWindow({
   close,
   nanny: { avatar_url, name },
 }) {
+   const [isOpen, setIsOpen] = useState(false);
+  const [selectedTime, setSelectedTime] = useState("");
+  
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+  const handleTimeSelect = (time) => {
+    setSelectedTime(time);
+    setIsOpen(false);
+  };
   const handleSubmit = (val, actions) => {
+    toast.success("Form submitted successfully!");
     close();
-    console.log(avatar_url, name);
-
     actions.resetForm();
   };
   const validationSchema = Yup.object({
@@ -21,7 +32,6 @@ export default function AppointmentModalWindow({
     email: Yup.string().email("Invalid email").required("Email is required"),
     parentName: Yup.string().required("Parent's name is required"),
     comment: Yup.string(),
-    meetingTime: Yup.string().required("Meeting time is required"),
     number: Yup.string()
       .matches(/^\+?3?8?(0\d{9})$/, "Invalid phone number format")
       .required("Number is required"),
@@ -50,7 +60,6 @@ export default function AppointmentModalWindow({
             email: "",
             parentName: "",
             comment: "",
-            meetingTime: "",
             number: "",
           }}
           validationSchema={validationSchema}
@@ -100,23 +109,27 @@ export default function AppointmentModalWindow({
                   />
                 </div>
                 <div className={css.clock}>
-                  <Field
-                    as="select"
-                    name="meetingTime"
-                    placeholder="00:00"
-                    className={clsx(css.field, css.fieldFirstFour)}
+                  <div
+                    className={clsx(css.field, css.fieldFirstFourClock)}
+                    onClick={toggleDropdown}
                   >
-                    <option value="">Meeting time</option>
-                  
-                      
-                    
-                  </Field><div className={css.clockContainer}>{timeOptions.map((time) => (
-                        <option key={time} value={time}>
+                    {selectedTime || "00:00"}{" "}
+                    <FaRegClock className={css.clockIcon} />
+                  </div>
+
+                  {isOpen && (
+                    <div className={css.clockContainer}>
+                      {timeOptions.map((time) => (
+                        <div
+                          key={time}
+                          className={css.option}
+                          onClick={() => handleTimeSelect(time)}
+                        >
                           {time}
-                        </option>
-                      ))}</div>
-                  <FaRegClock className={css.clockIcon} />
-                  
+                        </div>
+                      ))}
+                    </div>
+                  )}
                 </div>
               </div>
               <div>
